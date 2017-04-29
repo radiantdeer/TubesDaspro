706d7a265117b4e8c2e033035b78c4050d631786
nathannicho <nicholas.nathanael98@gmail.com>
2017-04-30 00:30:20 +0700

    Implementing function SudahJatuhTempo
    also comments addition and minor fixes

--- penarikan.pas
+++ penarikan.pas
@@ -1,9 +1,7 @@
 Program Penarikan;
 { Menarik sejumlah uang secara tunai dari suatu rekening tertentu }
 
-//Program belum menangani waktu jatuh tempo pada deposito dan tabungan rencana
-
-uses sysutils, banktype;
+uses sysutils, dateutils, banktype;
 
 { Kamus }
 var
@@ -18,6 +16,51 @@ var
 	stop, found, success : boolean;
 	N : integer;
 	i : integer; { Iterasi }
+
+function SudahJatuhTempo (rekonline : rekonline) : boolean;
+{ Menghasilkan true jika telah memenuhi waktu jatuh tempo dan false jika
+  belum memenuhi waktu jatuh tempo }
+{ Waktu jatuh tempo dihitung dari tanggal pembuatan rekening ke jangka 
+  waktu yang ditentukan untuk rekening tersebut }
+{ Kamus }
+var
+	tanggalMulai, jatuhTempo : TDateTime;
+	waktu : integer;
+	cmd : integer;
+	date, month, year : string;
+	dd, mm, yy : integer;
+	i : integer; { Iterasi }
+{ Algoritma }
+begin
+	date:=copy(rekonline.tglmulai,1,2);
+	month:=copy(rekonline.tglmulai,4,2);
+	year:=copy(rekonline.tglmulai,7,4);
+	val(date,dd);
+	val(month,mm);
+	val(year,yy);
+	tanggalMulai:=EncodeDate(yy,mm,dd);
+	case rekonline.waktu of
+		'1 bulan' : begin
+						waktu:=1;
+					end;
+		'3 bulan' : begin
+				 		waktu:=3;
+					end;
+		'6 bulan' : begin
+						waktu:=6;
+					end;
+		'1 tahun' : begin
+						waktu:=12;
+					end;
+	end;
+	jatuhTempo:=tanggalMulai;
+	for i:=1 to waktu do
+	begin
+		jatuhTempo:=IncMonth(jatuhTempo,1);
+	end;
+	cmd:=CompareDate(jatuhTempo,Now);
+	SudahJatuhTempo:=cmd<=0;
+end;
 	
 { Algoritma }
 begin
@@ -25,6 +68,9 @@ begin
 	writeln('> 1. Deposito');
 	writeln('> 2. Tabungan rencana');
 	writeln('> 3. Tabungan mandiri');
+	{ Validasi masukan jenis rekening }
+	{ Pengulangan akan berhenti jika pengguna memasukkan jenis rekening 
+	  yang tepat, yaitu 1, 2, atau 3 }
 	stop:=false;
 	repeat
 		write('> Jenis rekening : ');
@@ -34,6 +80,10 @@ begin
 		else
 			writeln('> Jenis rekening yang Anda masukkan salah!');
 	until stop;
+	{ Mendaftarkan nomor akun rekening-rekening milik pengguna ke array 
+	  tempArray sesuai dengan masukan jenis rekening }
+	{ N adalah banyaknya rekening milik pengguna dengan jenis tertentu
+	  sesuai masukan pengguna }
 	if (jenisRek=1) then
 	begin
 		jenis:='deposito';
@@ -73,6 +123,8 @@ begin
 	end;
 	if (N>0) then
 	begin
+		{ Menampilkan nomor akun rekening-rekening yang tersimpan pada
+		  array tempArray }
 		writeln('> Pilih rekening ',jenis,' Anda:');
 		for i:=1 to N do
 		begin
@@ -83,37 +135,41 @@ begin
 		{ Asumsikan nomor akun yang dimasukkan pengguna benar }
 		found:=false;
 		i:=1;
-		while (not(found)) do
+		while (i<=arrrekonline.Neff) and (not(found)) do
 		begin
 			if (arrrekonline.list[i].noakun=noAk) then
 				found:=true
 			else
 				i:=i+1;
 		end;
-		write('> Masukkan jumlah uang yang diinginkan!');
-		readln(jumlahTarik);
-		if (jenis='tabungan mandiri') and (arrrekonline.list[i].saldo>=jumlahTarik) then
+		if found then
 		begin
-			arrrekonline.list[i].saldo:=arrrekonline.list[i].saldo-jumlahTarik;
-			success:=true;
-		end else if (jenis='deposito') or (jenis='tabungan rencana') and (arrrekonline.list[i].saldo>=jumlahTarik) then //Ubah di sini
-		begin
-			arrrekonline.list[i].saldo:=arrrekonline.list[i].saldo-jumlahTarik;
-			success:=true;
-		end else
-			success:=false;
-		if (success=true) then
-		begin
-			arrtransaksi.Neff:=arrtransaksi.Neff+1;
-			arrtransaksi.list[arrtransaksi.Neff].noakun:=arrrekonline.list[i].noakun;
-			arrtransaksi.list[arrtransaksi.Neff].jenis:='penarikan';
-			arrtransaksi.list[arrtransaksi.Neff].uang:=arrrekonline.list[i].uang;
-			arrtransaksi.list[arrtransaksi.Neff].jumlah:=jumlahTarik;
-			arrtransaksi.list[arrtransaksi.Neff].saldoakhir:=arrrekonline.list[i].saldo;
-			arrtransaksi.list[arrtransaksi.Neff].tgl:=DateTimeToStr(Now);
-			writeln('> Penarikan berhasil! Jumlah saldo Anda adalah ',arrrekonline.list[i].saldo);
-		end else
-			writeln('> Anda tidak dapat melakukan penarikan.');
-	end else
+			write('> Masukkan jumlah uang yang diinginkan!');
+			readln(jumlahTarik);
+			if (jenis='tabungan mandiri') and (arrrekonline.list[i].saldo>=jumlahTarik) then
+			begin
+				arrrekonline.list[i].saldo:=arrrekonline.list[i].saldo-jumlahTarik;
+				success:=true;
+			end else if ((jenis='deposito') or (jenis='tabungan rencana')) and (arrrekonline.list[i].saldo>=jumlahTarik) and SudahJatuhTempo(arrrekonline.list[i]) then
+			begin
+				arrrekonline.list[i].saldo:=arrrekonline.list[i].saldo-jumlahTarik;
+				success:=true;
+			end else
+				success:=false;
+			if (success=true) then
+			begin
+				arrtransaksi.Neff:=arrtransaksi.Neff+1;
+				arrtransaksi.list[arrtransaksi.Neff].noakun:=arrrekonline.list[i].noakun;
+				arrtransaksi.list[arrtransaksi.Neff].jenis:='penarikan';
+				arrtransaksi.list[arrtransaksi.Neff].uang:=arrrekonline.list[i].uang;
+				arrtransaksi.list[arrtransaksi.Neff].jumlah:=jumlahTarik;
+				arrtransaksi.list[arrtransaksi.Neff].saldoakhir:=arrrekonline.list[i].saldo;
+				arrtransaksi.list[arrtransaksi.Neff].tgl:=FormatDateTime('DD-MM-YYYY',Now);
+				writeln('> Penarikan berhasil! Jumlah saldo Anda adalah ',arrrekonline.list[i].saldo);
+			end else { not(success) }
+				writeln('> Anda tidak dapat melakukan penarikan.');
+		end else { not(found) }
+			writeln('Rekening tidak ditemukan!');
+	end else { N=0 }
 		writeln('> Anda tidak mempunyai ',jenis,'.');
 end.
