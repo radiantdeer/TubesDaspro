8a6fa06de90bf3555bc35c20d7f4041c2a2c4570
robertsenps <robertsenputrasugianto@gmail.com>
2017-04-30 20:29:39 +0700

    Tinggal isUserExist yang belum

--- upembelian.pas
+++ upembelian.pas
@@ -1,30 +1,91 @@
-program progpembelian;
-uses sysutils,banktype;
+program pembelian;
+{Program untuk melakukan transaksi pembelian}
+uses sysutils,banktype,dateutils;
 
+{Kamus Global}
 var
-	listbrg : lbarang;
-	i : integer;
-	pil : integer;
-	neff : integer;
-	listpembelian : lpembelian;
-	rekening : rekonline;
-	jumlahBayar : real;
-	arrrekonline : lrekonline;
-	tempArray : array [1..50] of string;
-	listkurs : lkurs;
-	currentuser : nasabah;
-	
+	listbrg 		: lbarang;      //type barang yang dibeli     
+	pil 			: integer;		//variabel pilih menu
+	listpembelian 	: lpembelian;	//type histori pembelian
+	arrrekonline 	: lrekonline;	//type rekening online
+	listkurs 		: lkurs;		//type kurs yang dipakai
+	currentuser 	: nasabah;		//data nasabah
+
+{Deklarasi dan Spesifikasi Fungsi}
+function SudahJatuhTempo (rekonline : rekonline;jenis : string) : boolean;
+{ Menghasilkan true jika telah memenuhi waktu jatuh tempo dan false jika
+  belum memenuhi waktu jatuh tempo }
+{ Waktu jatuh tempo dihitung dari tanggal pembuatan rekening ke jangka 
+  waktu yang ditentukan untuk rekening tersebut }
+
+{ Kamus Lokal }
+var
+	tanggalMulai, jatuhTempo : TDateTime;
+	cmd : integer;
+	date, month, year : string;
+	dd, mm, yy : integer;
+{ Algoritma }
+begin
+	date:=copy(rekonline.tglmulai,1,2);		//Menyalin tanggal dari array rekonline
+	month:=copy(rekonline.tglmulai,4,2);	//Menyalin bulan dari array rekonline
+	year:=copy(rekonline.tglmulai,7,4);		//Menyalin tahun dari array rekonline
+	val(date,dd);							//Mengubah bentuk tanggal dari string to integer
+	val(month,mm);							//Mengubah bentuk bulan dari string to integer
+	val(year,yy);							//Mengubah bentuk tahun dari string to integer
+	tanggalMulai:=EncodeDate(yy,mm,dd);		//Mengubah bentuk yy mm dd menjadi type tanggal
+	if (jenis = 'deposito') then
+	begin
+		case rekonline.waktu of				//case waktu deposito dan mengincrement bulan sebesar waktunya
+			'1 bulan' : jatuhTempo:=IncMonth(tanggalMulai,1);
+			'3 bulan' : jatuhTempo:=IncMonth(tanggalMulai,3);
+			'6 bulan' : jatuhTempo:=IncMonth(tanggalMulai,6);
+			'1 tahun' : jatuhTempo:=IncMonth(tanggalMulai,12);
+		end;
+	end else
+	if (jenis = 'tabungan rencana') then	//case waktu tabungan rencana dan mengincrement tahun sebesar waktunya
+	begin
+		case rekonline.waktu of
+			'1 tahun' : jatuhTempo:=IncYear(tanggalMulai,1);	
+			'2 tahun' : jatuhTempo:=IncYear(tanggalMulai,2);
+			'3 tahun' : jatuhTempo:=IncYear(tanggalMulai,3);
+			'4 tahun' : jatuhTempo:=IncYear(tanggalMulai,4);
+			'5 tahun' : jatuhTempo:=IncYear(tanggalMulai,5);	
+			'6 tahun' : jatuhTempo:=IncYear(tanggalMulai,6);
+			'7 tahun' : jatuhTempo:=IncYear(tanggalMulai,7);
+			'8 tahun' : jatuhTempo:=IncYear(tanggalMulai,8);
+			'9 tahun' : jatuhTempo:=IncYear(tanggalMulai,9);	
+			'10 tahun' : jatuhTempo:=IncYear(tanggalMulai,10);
+			'11 tahun' : jatuhTempo:=IncYear(tanggalMulai,11);
+			'12 tahun' : jatuhTempo:=IncYear(tanggalMulai,12);
+			'13 tahun' : jatuhTempo:=IncYear(tanggalMulai,13);
+			'14 tahun' : jatuhTempo:=IncYear(tanggalMulai,14);	
+			'15 tahun' : jatuhTempo:=IncYear(tanggalMulai,15);
+			'16 tahun' : jatuhTempo:=IncYear(tanggalMulai,16);
+			'17 tahun' : jatuhTempo:=IncYear(tanggalMulai,17);
+			'18 tahun' : jatuhTempo:=IncYear(tanggalMulai,18);	
+			'19 tahun' : jatuhTempo:=IncYear(tanggalMulai,19);
+			'20 tahun' : jatuhTempo:=IncYear(tanggalMulai,20);
+		end;
+	end;
+	cmd:=CompareDate(jatuhTempo,Now);					//membandingkan tanggal setelah di increment dengan waktu sekarang
+	SudahJatuhTempo:=cmd<=0;							
+end;
+
 function gantikurs (awal : string; akhir : string;saldo : real) : real;
+{Mengganti kurs dari kurs awal ke kurs akhir dengan skala yang telah ditentukan}
+
+{Kamus Lokal}
 var
-	i : integer;
 	found : boolean;
 	temp : real;
+	i : integer;
 	
+{Algoritma Fungsi}
 begin
 	found := false;
 	i := 0;
-	while not(found) do
-	begin
+	while not(found) do				//Pencarian tabel kurs yang tepat
+	begin	
 		i := i +1;
 		if (akhir = listkurs.list[i].awal) and (akhir = listkurs.list[i].akhir) then
 		begin
@@ -35,7 +96,33 @@ begin
 	gantikurs := temp;
 end;
 
-procedure bayar(harga : real;arr : integer;nomor : string);
+function isUserExists(nonasabah : string; T : lnasabah) : integer;
+{Mengembalikan indeks array yang berisi nasabah yang dimaksud}
+
+{Kamus Lokal}
+var
+	i : integer;
+	found : boolean;
+
+{Algoritma Fungsi}
+begin
+    // Inisialisasi Variabel
+    i := 1;
+    found := false;
+    while((i <= T.Neff) AND NOT(found)) do // Bagian pencarian user
+    begin
+		if (T.list[i].nonasabah = nonasabah) then found := true
+		else i := i + 1;
+    end;
+    if (found) then isUserExists := i // User ditemukan
+    else isUserExists := 0; // User tidak ditemukan
+  end;
+
+
+procedure beli(harga : real;arr : integer;nomor : string);
+{Prosedur untuk membeli sesuatu dan mengurangkan saldo dengan harga barang}
+
+{Kamus Lokal}
 var
 	jenisRek : integer;
 	stop,success : boolean;
@@ -46,18 +133,21 @@ var
 	N : integer;
 	noAk : string;
 	found : boolean;
-	
+
+{Algoritma Prosedur}
 begin
-	writeln('> Pilih jenis rekening:');
+	writeln('> Pilih jenis rekening');
 	writeln('> 1. Deposito');
 	writeln('> 2. Tabungan rencana');
 	writeln('> 3. Tabungan mandiri');
+	{ Validasi masukan jenis rekening }
+	{ Pengulangan akan berhenti jika pengguna memasukkan jenis rekening 
+	  yang tepat, yaitu 1, 2, atau 3 }
 	stop:=false;
-	
 	repeat
 		write('> Jenis rekening : ');
 		readln(jenisRek);
-		if (jenisRek=1) or (jenisRek=2) or (jenisRek=3) then
+		if (jenisRek>= 1) and (jensRek <=3) then
 		begin
 			stop:=true;
 		end else
@@ -65,7 +155,10 @@ begin
 			writeln('> Jenis rekening yang Anda masukkan salah!');
 		end;
 	until stop;
-	
+	{ Mendaftarkan nomor akun rekening-rekening milik pengguna ke array 
+	  tempArray sesuai dengan masukan jenis rekening }
+	{ N adalah banyaknya rekening milik pengguna dengan jenis tertentu
+	  sesuai masukan pengguna }
 	if (jenisRek=1) then
 	begin
 		jenis:='deposito';
@@ -116,7 +209,7 @@ begin
 		{ Asumsikan nomor akun yang dimasukkan pengguna benar }
 		found:=false;
 		i:=1;
-		while (not(found)) do
+		while (i<=arrrekonline.Neff) and (not(found)) do
 		begin
 			if (arrrekonline.list[i].noakun=noAk) then
 			begin
@@ -134,7 +227,7 @@ begin
 			begin
 				arrrekonline.list[i].saldo:=gantikurs('IDR',arrrekonline.list[i].uang,(arrrekonline.list[i].saldo-ganti));
 				success:=true;
-			end else if (jenis='deposito') or (jenis='tabungan rencana') and (arrrekonline.list[i].saldo>=harga) then //Ubah di sini
+			end else if (jenis='deposito') or (jenis='tabungan rencana') and (arrrekonline.list[i].saldo>=harga) and SudahJatuhTempo(arrrekonline.list[i],jenis) then 
 			begin
 				arrrekonline.list[i].saldo:=gantikurs('IDR',arrrekonline.list[i].uang,(arrrekonline.list[i].saldo-ganti));
 				success:=true;
@@ -146,9 +239,8 @@ begin
 			begin
 				arrrekonline.list[i].saldo:=arrrekonline.list[i].saldo-harga;
 				success:=true;
-			end else if (jenis='deposito') or (jenis='tabungan rencana') and (arrrekonline.list[i].saldo>=harga) then //Ubah di sini
+			end else if (jenis='deposito') or (jenis='tabungan rencana') and (arrrekonline.list[i].saldo>=harga) and SudahJatuhTempo(arrrekonline.list[i],jenis) then //Ubah di sini
 			begin
-				if()
 				arrrekonline.list[i].saldo:=arrrekonline.list[i].saldo-harga;
 				success:=true;
 			end else
@@ -196,11 +288,10 @@ begin
 	writeln('> Masukkan nomor tujuan :');
 	write('> ');
 	readln(nomor);
-	bayar(listbrg.list[pil].harga,pil,nomor);
+	beli(listbrg.list[pil].harga,pil,nomor);
 end;
 
 {ALGORITMA UTAMA}
 begin
 	menu;
 end.
-	
