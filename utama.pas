a83181f1026f27d393c7468be57b471873a7df11
nathannicho <nicholas.nathanael98@gmail.com>
2017-05-02 02:24:52 +0700

    Penambahan unit ubukatutuprekening

--- utama.pas
+++ utama.pas
@@ -1,7 +1,7 @@
 Program InterfaceXYZ;
 // Program utama yang menangani berbagai masukan dari user
 
-uses uload, ulogin, ututup, ulihatrek, utransaksi, uadminnasabah, utampilanpengguna, uexit, banktype, sysutils, crt;
+uses uload, ulogin, ubukatutuprekening, ututup, ulihatrek, utransaksi, uadminnasabah, utampilanpengguna, uexit, banktype, sysutils, crt;
 // u<str> : Unit yang memuat fungsi-fungsi yang berkaitan dengan <str> bank
 // sysutils : Agar bisa memakai fungsi waktu yang disediakan Pascal
 // crt : Untuk clrscr
